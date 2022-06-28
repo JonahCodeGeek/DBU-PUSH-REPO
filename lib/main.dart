454bb2acc531b02a8ef627acf786d107a6998cc3
anonymousdomain <dawityitagesu@gmail.com>
2022-06-28 11:09:25 +0300

    name your source file with lowercase_with_underscores ' try to eliminate every and possible errors or warning you got that way it wil not get messy to find a bug '

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:dbu_push/screens/main-screen.dart';
+import 'package:dbu_push/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
