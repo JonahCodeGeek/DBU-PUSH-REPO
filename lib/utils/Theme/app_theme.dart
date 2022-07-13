f305ba773de2c418ee983a0fdb2a5f6f578a22f2
anonymousdomain <dawityitagesu@gmail.com>
2022-07-13 21:04:29 +0300

    merged from final

--- lib/utils/Theme/app_theme.dart
+++ lib/utils/Theme/app_theme.dart
@@ -1,6 +1,5 @@
 import 'package:dbu_push/utils/Theme/app_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 // App both dark and light theme will be shown here
 
