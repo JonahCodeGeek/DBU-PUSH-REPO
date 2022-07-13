31940601b4908d071ace8f9260be730e8d64a779
Yonas Legesse <yonaslegesse19@gmail.com>
2022-07-13 21:35:37 +0300

    added the slider functionality

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,35 +1,33 @@
-import 'package:dbu_push/widgets/app_text.dart';
+import 'package:dbu_push/screens/welcome%20screen/welcome_1.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'welcome screen/welcome_2.dart';
+import 'welcome screen/welcome_3.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final List screens = [
+    WelcomeScreenOne(),
+    WelcomeScreenTwo(),
+    WelcomeScreenThree(),
+  ];
 //stream builder will be returned here.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          color: Colors.white,
-          height: double.maxFinite,
-          width: double.maxFinite,
-          child: Column(
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              AppText.HeaderText('Title text'),
-              AppText.ContentText(
-                  'This is the paragraph text This is the paragraph textThis is the paragraph text'
-                  'This is the paragraph text This is the paragraph text This is the paragraph text'
-                  'This is the paragraph text'),
-              AppText.ReferenceText(
-                  'This will be a for usernames like @Jonah_code'),
-              ElevatedButton(
-                  onPressed: () => {},
-                  child:
-                      Text('This is to see that the primary color is working.'))
-            ],
-          ),
-        ),
-      ),
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    return PageView.builder(
+      scrollDirection: Axis.vertical,
+      physics: BouncingScrollPhysics(),
+      itemCount: screens.length,
+      itemBuilder: (_, index) {
+        return (screens[index]);
+      },
     );
   }
 }
---
+++ lib/screens/welcome screen/welcome_3.dart
@@ -0,0 +1,18 @@
+import 'package:dbu_push/widgets/app_text.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreenThree extends StatelessWidget {
+  const WelcomeScreenThree({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      height: double.maxFinite,
+      width: double.maxFinite,
+      child: SafeArea(
+        child: Column(),
+      ),
+    );
+  }
+}
