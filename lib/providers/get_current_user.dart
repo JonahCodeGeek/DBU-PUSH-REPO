97399d1a322174a596860373ad04fc334e82acd7
anonymousdomain <dawityitagesu@gmail.com>
2022-08-14 10:27:51 +0300

    provider class #dont forget this point of commit

---
+++ lib/providers/get_current_user.dart
@@ -0,0 +1,21 @@
+import 'package:dbu_push/models/user.dart';
+import 'package:dbu_push/utils/helpers/firestore_cloud_reference.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+
+class GetCurrentUser with ChangeNotifier {
+  UserModel? _currentUser;
+  UserModel? get currentUser => _currentUser;
+  void getCurrentUser(AsyncSnapshot<User?> snapshot) {
+    final doc = usersDoc.where('email', isEqualTo: snapshot.data?.email).get();
+    doc.then(
+      (snapshot) => {
+        snapshot.docs.forEach((element) async {
+          // await usersDoc.doc(element.id).get();
+          _currentUser = UserModel.fromDocument(element);
+          notifyListeners();
+        })
+      },
+    );
+  }
+}
